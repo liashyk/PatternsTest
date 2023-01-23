8e521c42df250f2cbc41ae2b8427b052852a3ed3
liashyk <ilya9may@gmail.com>
2023-01-23 21:30:17 +0200

    Make bridge

---
+++ Bridge/Abstraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+	internal class Abstraction
+	{
+		public int SomeProperty { get; set; }
+
+		private ISomethingGetter _somethingGetter;
+
+		public Abstraction(ISomethingGetter somethingGetter)
+		{
+			_somethingGetter=somethingGetter;
+		}
+
+		public void DoSomething()
+		{
+			Console.WriteLine( _somethingGetter.GetSomething());
+		}
+	}
+}
---
+++ Bridge/TomatoGetter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+	internal class TomatoGetter : ISomethingGetter
+	{
+		public string GetSomething()
+		{
+			return "i give you tomato";
+		}
+	}
+}
