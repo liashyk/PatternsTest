e0d9c35084c59b87322cf528b500dbf58f4270ac
liashyk <ilya9may@gmail.com>
2023-01-23 14:03:08 +0200

    add factory

---
+++ Factory/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Factory
+{
+	internal class Program
+	{
+		static void Run()
+		{
+			Console.WriteLine("Choose place:");
+			Console.WriteLine("1.Sea");
+			Console.WriteLine("2.Ground");
+
+			int placeNumber = 0;
+
+			string? placeNumberBuffer = Console.ReadLine();
+
+			while (!int.TryParse(placeNumberBuffer, out placeNumber)
+				|| placeNumber < 1 || placeNumber > 2)
+			{
+				Console.WriteLine("Something Wrong. Try again");
+				placeNumberBuffer = Console.ReadLine();
+			}
+
+			IFactory factory;
+			switch (placeNumber)
+			{
+				case 1:
+					factory = new SeaFactory();
+					break;
+				case 2:
+					factory = new GroundFactory();
+					break;
+				default:
+					factory = new GroundFactory();
+					break;
+			}
+
+			ITransport transport = factory.GetTransport();
+
+			Console.WriteLine($"You get {transport.GetType()}");
+			Console.WriteLine(transport.Deliver());
+		}
+		static void Main(string[] args)
+		{
+			Run();
+		}
+	}
+}
