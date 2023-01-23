21f5e3358e151b19505aa068f8245e3935f81f18
liashyk <ilya9may@gmail.com>
2023-01-23 22:54:18 +0200

    make facade

---
+++ Facade/Facade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+	internal class Facade
+	{
+		private VeryComplicatedService service;
+
+		public Facade()
+		{
+			service = new VeryComplicatedService();
+		}
+		public void MakeOperation()
+		{
+			service.MakeComplicateThing();
+			service.MakeAnotherComplicateThing();
+			service.MakeComplcateThingButEasierThatBefore();
+			service.MakeTheMostComplicatedThing();
+		}
+	}
+}
---
+++ Facade/VeryComplicatedService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+	internal class VeryComplicatedService
+	{
+		public void MakeComplicateThing() { }
+
+		public void MakeAnotherComplicateThing() { }
+
+		public void MakeComplcateThingButEasierThatBefore() { }
+
+		public void MakeTheMostComplicatedThing() { }
+
+	}
+}
