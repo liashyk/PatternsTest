a58ed4b0ea44c2cad7dc30317d124fd73ad34ce0
liashyk <ilya9may@gmail.com>
2023-01-23 20:05:41 +0200

    make prototype

---
+++ Prototype/Airplane.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+	internal class Airplane
+	{
+		public int WingsAmount { get; set; }
+		public string Color { get; set; }
+		private string _technologyStuff;
+		public string TechnologyStuff => _technologyStuff;
+		public Airplane(int wingsAmount, string color)
+		{
+			WingsAmount = wingsAmount;
+			Color = color;
+			_technologyStuff = "nothing";
+		}
+
+		public void MakeTechlogyStuff()
+		{
+			_technologyStuff = "now it is must work";
+		}
+
+		public Airplane Clone()
+		{
+			return (Airplane)MemberwiseClone();
+		}
+	}
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototype
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Airplane airplane1 = new Airplane(2, "red");
+			airplane1.MakeTechlogyStuff();
+			Airplane airplane2= airplane1.Clone();
+			Console.WriteLine($"They are the same object?: {airplane1==airplane2}");
+			Console.WriteLine($"They have now same private technology stuff? : " +
+				$"{ airplane1.TechnologyStuff==airplane2.TechnologyStuff}");
+		}
+	}
+}
