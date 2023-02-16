8d3b9a12596c08d16e9b73e1c84b8af676b5fdf1
liashyk <ilya9may@gmail.com>
2023-02-17 00:58:44 +0200

    make CoR

---
+++ CoR/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace CoR
+{
+	internal class Program
+	{
+		interface IHandler
+		{
+			void Handle(string message);
+		}
+
+		abstract class Handler : IHandler
+		{
+			private IHandler? next;
+
+			public void AddNext(IHandler handler)
+			{
+				next = handler;
+			}
+
+			public virtual void Handle(string message)
+			{
+				if (next != null && message!=null)
+				{
+					next.Handle(message);
+				}
+			}
+		}
+
+		class AuthorizeHandler : Handler
+		{
+			public override void Handle(string message)
+			{
+				if (message == "oops")
+				{
+					Console.WriteLine("Do Authorize");
+				}
+				else
+				{
+					base.Handle(message);
+				}
+			}
+		}
+
+		class AutenficationHandler : Handler
+		{
+			public override void Handle(string message)
+			{
+				if (message == "opa")
+				{
+					Console.WriteLine("Do Authorize");
+				}
+				else
+				{
+					base.Handle(message);
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Handler authorize = new AuthorizeHandler();
+			Handler autenfication = new AutenficationHandler();
+			autenfication.AddNext(authorize);
+			autenfication.Handle("oops");
+		}
+	}
+}
