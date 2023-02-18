a9108b3552f5e0fc3fdd9e91fd8b11785cfb89fa
liashyk <ilya9may@gmail.com>
2023-02-18 21:46:52 +0200

    mediator and command

---
+++ Command/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Command
+{
+	internal class Program
+	{
+		interface ICommand
+		{
+			public void Execute();
+		}
+
+		class ConcretteComand:ICommand
+		{
+			private string _parameteres;
+
+			private Receiver _receiver;
+
+			public ConcretteComand(string parameteres, Receiver receiver)
+			{
+				_parameteres= parameteres;
+				_receiver= receiver;
+			}
+
+			public void Execute()
+			{
+				_receiver.MakeSomething();
+				_receiver.MakeAnything();
+				Console.WriteLine(_parameteres);
+			}
+		}
+
+		class SecondComand : ICommand
+		{
+			private Receiver _receiver;
+			public SecondComand(Receiver receiver)
+			{
+				_receiver= receiver;
+			}
+			public void Execute()
+			{
+				_receiver.MakeSomething();
+			}
+		}
+
+		class Receiver
+		{
+			public void MakeSomething()
+			{
+				Console.WriteLine("make smth");
+			}
+
+			public void MakeAnything()
+			{
+				Console.WriteLine("make anything");
+			}
+		}
+
+		class Invoker
+		{
+			private ICommand _command;
+			public void SetCommand(ICommand command)
+			{
+				_command = command;
+			}
+
+			public void ExecuteComand()
+			{
+				_command.Execute();
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var invoker = new Invoker();
+			var receiver = new Receiver();
+			ICommand command1 = new ConcretteComand("oops", receiver);
+			ICommand command2 = new SecondComand(receiver);
+			invoker.SetCommand(command1);
+			invoker.ExecuteComand();
+			Console.WriteLine("////////");
+			invoker.SetCommand(command2);
+			invoker.ExecuteComand();
+
+		}
+	}
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Mediator
+{
+	internal class Program
+	{
+		interface IMediator
+		{
+			public void Notify(Component sender, string eventName);
+		}
+
+		abstract class Component
+		{
+			protected IMediator? _dialog;
+
+			public Component(IMediator? dialog)
+			{
+				_dialog = dialog;
+			}
+
+			public virtual void Click()
+			{
+				Console.WriteLine("component was clicked");
+			}
+
+		}
+
+		class Button : Component
+		{
+			public Button(IMediator? dialog) : base(dialog)
+			{
+			}
+
+			public override void Click()
+			{
+				Console.WriteLine("button was clicked");
+				_dialog.Notify(this, "click");
+			}
+		}
+
+		class TextField : Component
+		{
+
+			public TextField(IMediator? dialog) : base(dialog)
+			{
+			}
+
+			public override void Click()
+			{
+				Console.WriteLine("TextField was clicked");
+				_dialog.Notify(this, "click");
+			}
+
+			public void ShowText()
+			{
+				Console.WriteLine("text was shown");
+			}
+		}
+
+		class Dialog : IMediator
+		{
+			private TextField textField;
+			private Button button;
+
+			public Dialog()
+			{
+				textField = new TextField(this);
+				button = new Button(this);
+			}
+
+			public void Notify(Component sender, string eventName)
+			{
+				if(sender== button && eventName=="click") 
+				{ 
+					textField.ShowText();
+				}
+			}
+		}
+
+
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
